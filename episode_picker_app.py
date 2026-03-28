8784fb42c00a36f3f8a082fe0dc496b8a0f9c87d
Prince Emili <princeemili78@gmail.com>
2026-03-28 09:05:26 -0400

    fix session state and conditional rendering

--- episode_picker_app.py
+++ episode_picker_app.py
@@ -7,6 +7,9 @@ st.title("Tv Show Randomizer")
 
 
 if "show" not in st.session_state:
+    st.session_state["show"] = ""
+    
+if "show_name" not in st.session_state:   
     st.session_state["show_name"] = ""
 
 
@@ -15,13 +18,13 @@ show_name = st.text_input("Name of Tv Show")
 
 
 # create instance of tv show using user input
-if "show" not in st.session_state or st.session_state["show_name"] != show_name:
+if show_name != "" and "show" not in st.session_state or st.session_state["show_name"] != show_name :
     st.session_state["show"] = TvShow(show_name)
     st.session_state["last_input"] = show_name
+        # number input for ratings you wanna watch
 
-# number input for ratings you wanna watch
-rating = st.number_input("Lowest rated episode you want to watch", min_value=0.0, max_value=10.0, value=None, format="%0.1f")
-seasons = st.multiselect("Which seasons do you want to choose from", options=range(1, 1 + st.session_state["show"].num_seasons))
-
-if st.button("Generate episode!") == True:
-    st.write(st.session_state["show"].random_episode(rating, seasons))
+if isinstance(st.session_state["show"], TvShow):         
+    rating = st.number_input("Lowest rated episode you want to watch", min_value=0.0, max_value=10.0, value=None, format="%0.1f")
+    seasons = st.multiselect("Which seasons do you want to choose from", options=range(1, 1 + st.session_state["show"].num_seasons))
+    if st.button("Generate episode!") == True:
+        st.write(st.session_state["show"].random_episode(rating, seasons))
